50b2c1141cefe91cdbe2dbd9b6117e7d961ccfed
Vito Cairone <vitocairone@gmail.com>
2016-04-14 16:31:41 -0700

    add questions controller + new question view

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,19 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
   root 'pages#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
   get '/auth/:provider/callback' => 'sessions#create'
 
   delete '/logout' => 'sessions#destroy'
+
+  resources :questions
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
